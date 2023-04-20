26f232e7ae85539e5d7064e3493449b61bffa17e
Maxalbinsson <max.albiknsson@elev.ga.ntig.se>
2023-04-20 13:35:20 +0200

    lagt till den som ska vara mid på korten

---
+++ src/kort.java
@@ -0,0 +1,24 @@
+public class kort {
+    int hp;
+    int dmg;
+    int cost;
+    int mana;
+
+    public int getHp(){
+            return hp;
+    }
+
+    public int getDmg(){
+        return dmg;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+public int getMana(){
+        return mana;
+
+
+}
+
+}
